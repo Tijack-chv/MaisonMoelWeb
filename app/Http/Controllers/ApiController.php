556fc874cf63dba132d6747e938b1c9fcacb462d
mgaudre <mgaudre@PC2>
2024-12-03 13:43:06 +0100

    API -> login d'un serveur : renvoie l'objet serveur si les informations sont corrects sinon ERREUR

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Serveur;
+use Illuminate\Support\Facades\Hash;
+
+class ApiController extends Controller
+{
+    public function api_login(Request $request)
+    {
+        if (!$request->has('email') || !$request->has('password')) {
+            return response()->json(['error' => 'Email et mot de passe sont requis.']);
+        }
+        $email = htmlspecialchars($request->email);
+        $password = htmlspecialchars($request->password);
+        $serveur = Serveur::where('EMAIL', $email)->first();
+        if ($serveur) {
+            if (Hash::check($password, $serveur->PASSWORD)) {
+                return response()->json(['serveur' => $serveur]);
+            } else {
+                return response()->json(['error' => 'Email ou mot de passe incorrect.']);
+            }
+        } else {
+            return response()->json(['error' => 'Email ou mot de passe incorrect.']);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ApiController;
 
 Route::get('/', [PublicController::class, 'index'])->name('index');
 
@@ -12,3 +13,5 @@ Route::post('/login', [AuthController::class, 'login_verif'])->name('login.login
 Route::get('/register', [AuthController::class, 'register_view'])->name('register');
 Route::post('/register', [AuthController::class, 'register_store'])->name('register.register');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/api/login', [ApiController::class, 'api_login']);
