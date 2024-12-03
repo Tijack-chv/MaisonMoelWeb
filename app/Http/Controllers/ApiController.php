08c5f4516800ffe59d9699a4d884a81e66df12d0
mgaudre <mgaudre@PC2>
2024-12-03 13:52:38 +0100

    Sécurité pour l'api -> Ajout d'un token unique

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Serveur;
+use App\Models\Tokenapi;
 use Illuminate\Support\Facades\Hash;
 
 class ApiController extends Controller
@@ -12,18 +13,25 @@ class ApiController extends Controller
     {
         if (!$request->has('email') || !$request->has('password')) {
             return response()->json(['error' => 'Email et mot de passe sont requis.']);
+        } else if (!$request->has('token')) {
+            return response()->json(['error' => 'Token est requis.']);
         }
-        $email = htmlspecialchars($request->email);
-        $password = htmlspecialchars($request->password);
-        $serveur = Serveur::where('EMAIL', $email)->first();
-        if ($serveur) {
-            if (Hash::check($password, $serveur->PASSWORD)) {
-                return response()->json(['serveur' => $serveur]);
+        $token = htmlspecialchars($request->token);
+        if (Tokenapi::where('token', $token)->first()) {
+            $email = htmlspecialchars($request->email);
+            $password = htmlspecialchars($request->password);
+            $serveur = Serveur::where('EMAIL', $email)->first();
+            if ($serveur) {
+                if (Hash::check($password, $serveur->PASSWORD)) {
+                    return response()->json(['serveur' => $serveur]);
+                } else {
+                    return response()->json(['error' => 'Email ou mot de passe incorrect.']);
+                }
             } else {
                 return response()->json(['error' => 'Email ou mot de passe incorrect.']);
             }
         } else {
-            return response()->json(['error' => 'Email ou mot de passe incorrect.']);
+            return response()->json(['error' => 'Le token est invalide.']);
         }
     }
 }
