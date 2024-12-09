6f908ff9f20fdfe585b0b5324a064405c3874681
Mathis Gaudre <mathis.gaudre@gmail.com>
2024-12-09 13:01:14 +0100

    Responsive Profile

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<div class="px-[20rem]">
+<div class="lg:px-[12rem] md:px-[3rem]">
     <div class="mx-auto shadow-lg bg-[#292929] rounded py-6 px-[4rem] text-[#FFEB99]">
         {{ $slot }}
     </div>
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -15,7 +15,7 @@
                 @csrf
                 <div class="mb-4">
                     <label for="nom" class="block text-md text-[#FFEB99]">Nom du compte</label>
-                    <input type="text" value="{{$user['nom']}}" name="nom" id="nom" class="border-2 bg-transparent rounded w-1/2 py-2 px-3 text-[#FFEB99] placeholder-[#FFEB99]::placeholder focus:outline-none focus:border-[#FFEB99]">
+                    <input type="text" value="{{$user['nom']}}" name="nom" id="nom" class="border-2 bg-transparent rounded w-full md:w-1/2 py-2 px-3 text-[#FFEB99] placeholder-[#FFEB99]::placeholder focus:outline-none focus:border-[#FFEB99]">
                     @error('nom')
                         <div class="text-red-500 mt-2 text-sm">
                             {{ $message }}
@@ -24,7 +24,7 @@
                 </div>
                 <div class="mb-4">
                     <label for="prenom" class="block text-md text-[#FFEB99]">Prénom du compte</label>
-                    <input type="text" value="{{$user['prenom']}}" name="prenom" id="prenom" class="border-2 bg-transparent rounded w-1/2 py-2 px-3 text-[#FFEB99] placeholder-[#FFEB99]::placeholder focus:outline-none focus:border-[#FFEB99]">
+                    <input type="text" value="{{$user['prenom']}}" name="prenom" id="prenom" class="border-2 bg-transparent rounded w-full md:w-1/2 py-2 px-3 text-[#FFEB99] placeholder-[#FFEB99]::placeholder focus:outline-none focus:border-[#FFEB99]">
                     @error('prenom')
                         <div class="text-red-500 mt-2 text-sm">
                             {{ $message }}
@@ -33,7 +33,7 @@
                 </div>
                 <div class="mb-4">
                     <label for="email" class="block text-md text-[#FFEB99]">Email</label>
-                    <input type="email" value="{{ $user['email'] }}" name="email" id="email" class="border-2 bg-transparent rounded w-1/2 py-2 px-3 text-[#FFEB99] placeholder-[#FFEB99]::placeholder focus:outline-none focus:border-[#FFEB99]" value="{{ old('email') }}">
+                    <input type="email" value="{{ $user['email'] }}" name="email" id="email" class="border-2 bg-transparent rounded w-full md:w-1/2 py-2 px-3 text-[#FFEB99] placeholder-[#FFEB99]::placeholder focus:outline-none focus:border-[#FFEB99]" value="{{ old('email') }}">
                     @error('email')
                         <div class="text-red-500 mt-2 text-sm">
                             {{ $message }}
@@ -50,14 +50,14 @@
     </div>
 
 
-    <div class="mt-6">
+    <div class="mt-6 mb-6">
         <x-card>
             <h2 class="text-xl">Modification du mot de passe</h2>
             <form action="/profile/" method="post" class="pt-4">
                 @csrf
                 <div class="mb-4">
                     <label for="current_password" class="block text-md text-[#FFEB99]">Mot de passe actuel</label>
-                    <input placeholder="••••••••" type="password" name="current_password" id="current_password" class="border-2 bg-transparent rounded w-1/2 py-2 px-3 text-[#FFEB99] placeholder-[#FFEB99]::placeholder focus:outline-none focus:border-[#FFEB99]">
+                    <input placeholder="••••••••" type="password" name="current_password" id="current_password" class="border-2 bg-transparent rounded w-full md:w-1/2 py-2 px-3 text-[#FFEB99] placeholder-[#FFEB99]::placeholder focus:outline-none focus:border-[#FFEB99]">
                     @error('current_password')
                         <div class="text-red-500 mt-2 text-sm">
                             {{ $message }}
@@ -66,7 +66,7 @@
                 </div>
                 <div class="mb-4">
                     <label for="new_password" class="block text-md text-[#FFEB99]">Nouveau mot de passe</label>
-                    <input placeholder="••••••••" type="password" name="new_password" id="new_password" class="border-2 bg-transparent rounded w-1/2 py-2 px-3 text-[#FFEB99] placeholder-[#FFEB99]::placeholder focus:outline-none focus:border-[#FFEB99]">
+                    <input placeholder="••••••••" type="password" name="new_password" id="new_password" class="border-2 bg-transparent rounded w-full md:w-1/2 py-2 px-3 text-[#FFEB99] placeholder-[#FFEB99]::placeholder focus:outline-none focus:border-[#FFEB99]">
                     @error('new_password')
                         <div class="text-red-500 mt-2 text-sm">
                             {{ $message }}
@@ -75,7 +75,7 @@
                 </div>
                 <div class="mb-4">
                     <label for="new_password_confirmation" class="block text-md text-[#FFEB99]">Confirmation du mot de passe</label>
-                    <input placeholder="••••••••" type="password" name="new_password_confirmation" id="new_password_confirmation" class="border-2 bg-transparent rounded w-1/2 py-2 px-3 text-[#FFEB99] placeholder-[#FFEB99]::placeholder focus:outline-none focus:border-[#FFEB99]">
+                    <input placeholder="••••••••" type="password" name="new_password_confirmation" id="new_password_confirmation" class="border-2 bg-transparent rounded w-full md:w-1/2 py-2 px-3 text-[#FFEB99] placeholder-[#FFEB99]::placeholder focus:outline-none focus:border-[#FFEB99]">
                     @error('new_password_confirmation')
                         <div class="text-red-500 mt-2 text-sm">
                             {{ $message }}
