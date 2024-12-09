4664efb5316285bb832d4b2a9ebc1678644f37f3
Mathis Gaudre <mathis.gaudre@gmail.com>
2024-12-09 21:07:58 +0100

    Mise à jour du lien vers les images. Avant : 192.168.143.9/images ||| Après : 192.168.143.9:8080

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 <x-header title="bg-[#292929]">
     <x-navbar></x-navbar>
-    <div class="relative h-screen bg-cover bg-center" style="background-image: url('http://192.168.143.9/images/FOND_ACCUEIL.png');">
+    <div class="relative h-screen bg-cover bg-center" style="background-image: url('http://192.168.143.9:8080/images/FOND_ACCUEIL.png');">
         <div class="absolute inset-0 bg-black bg-opacity-50"></div>
         <div class="absolute flex w-screen h-screen sm:px-10" style="flex-direction: column;">
             <div class="mt-12">
