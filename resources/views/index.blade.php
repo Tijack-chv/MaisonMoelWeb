e7fdf740b21497332a7a2234124750181b1dc442
Mathis Gaudre <mathis.gaudre@gmail.com>
2024-12-10 19:07:56 +0100

    Mise à jour de la mise en page de la section "À propos de nous" et ajout de nouveaux avis clients avec un composant de carte de contact.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,7 @@
     </div>
 
     <div class="py-6 px-48">
-        <h1 class="place-self-center text-4xl md:text-5xl text-[#FFEB99] titre-font pb-9">
+        <h1 class="place-self-center text-center text-4xl md:text-5xl text-[#FFEB99] titre-font pb-9">
             À propos de nous
         </h1>
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-0">
@@ -36,10 +36,10 @@
         </div>
     </div>
 
-    <hr class="w-96 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700">
+    <hr class="w-96 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10">
     
     <div class="grid grid-cols-1 pt-2 pb-6">
-        <h1 class="place-self-center text-4xl md:text-5xl text-[#FFEB99] titre-font">
+        <h1 class="place-self-center text-center text-4xl md:text-5xl text-[#FFEB99] titre-font">
             Nos sélections
         </h1>
         <div class="pt-6 pb-2 md:px-[6rem] lg:px-[12rem]">
@@ -87,10 +87,10 @@
             </swiper-container>
         </div>
 
-        <hr class="w-96 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700">
+        <hr class="w-96 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10">
 
         <div class="pt-2 pb-6 px-48">
-            <h1 class="place-self-center text-4xl md:text-5xl text-[#FFEB99] titre-font pb-6">
+            <h1 class="place-self-center text-center text-4xl md:text-5xl text-[#FFEB99] titre-font pb-6">
                 Contactez-nous
             </h1>
             <div class="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-0">
@@ -116,9 +116,6 @@
                 </div>
             </div>
         </div>
-            
-
-
 
         <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
         <script>
@@ -139,15 +136,14 @@
                 .bindPopup('Camoël, France');
         </script>
         <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
-        </div>
 
-        <hr class="w-96 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700">
+        <hr class="w-96 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10">
 
         <div class="pt-2 pb-6 px-48">
-            <h1 class="place-self-center text-4xl md:text-5xl text-[#FFEB99] titre-font pb-6">
+            <h1 class="place-self-center text-center text-4xl md:text-5xl text-[#FFEB99] titre-font pb-6">
                 Vos avis
             </h1>
-            <x-card-contact title="mx-auto w-1/2">
+            <x-card-contact title="mx-auto w-full mb-8">
                 <div class="flex justify-center mb-2">
                     <div class="flex">
                         <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
@@ -206,7 +202,46 @@
                         <span class="text-sm font-medium text-gray-500 dark:text-gray-400">2%</span>
                     </div>
                 </div>
+            </x-card-contact>
 
+            <x-card-contact title="mx-auto w-full">
+                <article>
+                    <div class="flex items-center mb-4">
+                        <img class="w-10 h-10 me-4 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="">
+                        <div class="font-medium dark:text-white">
+                            <p>Jese Leos <time datetime="2014-08-16 19:00" class="block text-sm text-gray-500 dark:text-gray-400">Joined on August 2014</time></p>
+                        </div>
+                    </div>
+                    <div class="flex items-center mb-1 space-x-1 rtl:space-x-reverse">
+                        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                        </svg>
+                        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                        </svg>
+                        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                        </svg>
+                        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                        </svg>
+                        <svg class="w-4 h-4 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                        </svg>
+                        <h3 class="ms-2 text-sm font-semibold text-gray-900 dark:text-white">Thinking to buy another one!</h3>
+                    </div>
+                    <footer class="mb-5 text-sm text-gray-500 dark:text-gray-400"><p>Reviewed in the United Kingdom on <time datetime="2017-03-03 19:00">March 3, 2017</time></p></footer>
+                    <p class="mb-2 text-gray-500 dark:text-gray-400">This is my third Invicta Pro Diver. They are just fantastic value for money. This one arrived yesterday and the first thing I did was set the time, popped on an identical strap from another Invicta and went in the shower with it to test the waterproofing.... No problems.</p>
+                    <p class="mb-3 text-gray-500 dark:text-gray-400">It is obviously not the same build quality as those very expensive watches. But that is like comparing a Citroën to a Ferrari. This watch was well under £100! An absolute bargain.</p>
+                    <a href="#" class="block mb-5 text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Read more</a>
+                    <aside>
+                        <p class="mt-1 text-xs text-gray-500 dark:text-gray-400">19 people found this helpful</p>
+                        <div class="flex items-center mt-3">
+                            <a href="#" class="px-2 py-1.5 text-xs font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Helpful</a>
+                            <a href="#" class="ps-4 text-sm font-medium text-blue-600 hover:underline dark:text-blue-500 border-gray-200 ms-4 border-s md:mb-0 dark:border-gray-600">Report abuse</a>
+                        </div>
+                    </aside>
+                </article>
             </x-card-contact>
 
 </x-header>
